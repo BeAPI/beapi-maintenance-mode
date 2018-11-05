c9488b636189698889b133b282cd8b07767a73e6
Romain Lefort <rlefort@beapi.fr>
2018-11-05 18:53:18 +0100

    #7 Improve template style

--- templates/maintenance.php
+++ templates/maintenance.php
@@ -3,31 +3,47 @@
 <head>
 	<title>Service unavailable.</title>
 	<style>
+		html {
+			width: 100%;
+			height: 100%;
+		}
 		body {
-			background-color: #e0dcdc;
-			color: #444;
+			width: 100%;
+			height: 100%;
+			background: #232526;
+			background: -webkit-linear-gradient(to right, #414345, #232526);
+			background: linear-gradient(to right, #414345, #232526);
 			font-family: "Open Sans", sans-serif;
 			font-size: 13px;
 			line-height: 1.4em;
 		}
-
-		body > div {
-			background-color: #f8f8f8;
-			border: 1px solid #ddd;
-			border-radius: 5px;
-			box-shadow: 0 0 5px #888;
-			margin: 3em auto;
-			padding: 2em;
-			text-align: center;
-			width: 20%;
-			min-width: 20em;
+		.denied {
+			background: #f7f7f7;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate3d(-50%, -50%, 0);
+			padding: 50px;
+    		text-align: center;
+		}
+		.denied h1 {
+			font-size: 40px;
+    		line-height: 50px;
+		}
+		.denied p {
+			font-size: 16px;
+		}
+		.denied p.icon {
+			font-size: 30px;
+    		margin-bottom: 0px;
 		}
 	</style>
 </head>
 <body>
-<div>
-	<h1>Access Denied/Forbidden</h1>
-	<p>Please contact your webmaster ...</p>
-</div>
+	<div class="denied">
+		<h1>Access Denied/Forbidden</h1>
+		<p>Please contact your webmaster ...</p>
+		<p class="icon">🔐</p>
+	</div>
 </body>
 </html>
